5d41e6f45f34bbe44278fbdec4d7f186990a6cc9
mariosc9 <mario_sanchezcanas@iescarlesvallbona.cat>
2023-11-27 20:01:10 +0100

    error login

--- routes/auth.php
+++ routes/auth.php
@@ -20,21 +20,20 @@ Route::get('/auth/redirect', function () {
 Route::get('/auth/callback', function () {
     $googleUser = Socialite::driver('google')->user();
 
-    /*$user = User::updateOrCreate([
-        'id' => $googleUser->id,
-    ], [
-        'name' => $googleUser->name,
-        'email' => $googleUser->email,
-        //'remember_token' => $googleUser->token,
-        //'google_refresh_token' => $googleUser->refreshToken,
-    ]);*/
+    $user = User::where('email', $googleUser->email)->first();
 
-$user = User::where('email', $googleUser->email);
-if ($user){
+    /*  if (!$user) {
+        // User doesn't exist, create a new one
+        $user = User::create([
+            'name' => $googleUser->name,
+            'email' => $googleUser->email,
+            // Add any other necessary fields
+        ]);
+    }
+    */
     Auth::login($user);
-    return redirect('/dashboard');
-}
 
+    return redirect('/dashboard');
 });
 
 
