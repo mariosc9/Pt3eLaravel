b1bf107612743098d28f1ce6f0af1d9bef031e04
mariosc9 <mario_sanchezcanas@iescarlesvallbona.cat>
2023-11-27 19:13:18 +0100

    Google sign in millorat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,10 +16,7 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
@@ -34,9 +31,9 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                {{ __('Forgot your password?') }}
+            </a>
             @endif
 
             <x-primary-button class="ms-3">
@@ -45,5 +42,11 @@
         </div>
     </form>
 
-    <a href='google.redirect' type="button">Sign in with Google</a>
+    <a href="{{ route('google.login') }}" class="inline-flex items-center px-4 py-2 mt-2 text-sm font-medium text-white bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15H8.83L8 18h3v3h2v-3h3v-2h-3v-3h-2v3zm0-5v-2h4.17l.59-.59L15 9.76l-1.41-1.42-2.34 2.34L9.17 8H5v4h2v-1.83l2.42 2.42 2.34-2.34L14.24 12z" />
+        </svg>
+        Sign in with Google
+    </a>
+
 </x-guest-layout>
